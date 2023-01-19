914094c2ecb872e05974c772e60043851e48f5d6
Michael Burton <mjburton11@Michaels-MacBook-Pro.local>
2023-01-18 22:23:30 -0500

    add new inventory items

--- src/app/inventory.tsx
+++ src/app/inventory.tsx
@@ -1,13 +1,53 @@
+import { TextField } from '@mui/material'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
+import React, { useState } from 'react'
 import './App.css'
+
+type Item = {
+  name: string
+  quantity: number
+}
+
+const defaultItem = { name: '', quantity: 0 }
 
 export function Inventory() {
   const boldFont = { fontWeight: 'bold' }
+
+  const [items, setItems] = useState<Item[]>([
+    { name: 't-shirt', quantity: 3 },
+    { name: 'pants', quantity: 6 },
+  ])
+  const [newItem, setNewItem] = useState<Item>(defaultItem)
+  const [invalid, setInvalid] = useState({ name: false, quantity: false })
+
+  const updateNewName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewItem({ ...newItem, name: event.target.value })
+  }
+  const updateNewQuantity = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewItem({
+      ...newItem,
+      quantity: +event.target.value,
+    })
+  }
+  const saveNewItem = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      if (newItem.name !== '' && newItem.quantity !== 0) {
+        setItems([...items, newItem])
+        setNewItem(defaultItem)
+        setInvalid({ name: false, quantity: false })
+      }
+      setInvalid({
+        name: newItem.name === '',
+        quantity: newItem.quantity === 0,
+      })
+    }
+  }
+
   return (
     <main className="pi-page">
       <h1 className="pi-header">Inventory </h1>
@@ -22,7 +62,7 @@ export function Inventory() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {items.map((row) => (
               <TableRow
                 key={row.name}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -33,15 +73,60 @@ export function Inventory() {
                 <TableCell align="right">{row.quantity}</TableCell>
               </TableRow>
             ))}
+            <TableRow>
+              <TableCell>
+                <TextField
+                  sx={{
+                    fontSize: '14px',
+                    fontStyle: 'italic',
+                  }}
+                  fullWidth
+                  variant="standard"
+                  InputProps={{
+                    disableUnderline: !invalid.name,
+                    sx: {
+                      fontSize: '14px',
+                      fontStyle: 'italic',
+                    },
+                  }}
+                  id="new-item"
+                  placeholder="New item"
+                  size="small"
+                  margin="none"
+                  type="text"
+                  error={invalid.name}
+                  onChange={updateNewName}
+                  onKeyDown={saveNewItem}
+                  value={newItem.name}
+                ></TextField>
+              </TableCell>
+              <TableCell align="right">
+                <TextField
+                  variant="standard"
+                  fullWidth
+                  InputProps={{
+                    disableUnderline: !invalid.quantity,
+                    sx: {
+                      fontSize: '14px',
+                      fontStyle: 'italic',
+                      direction: 'rtl',
+                    },
+                  }}
+                  error={invalid.quantity}
+                  id="new-quantity"
+                  placeholder="Quantity"
+                  size="small"
+                  margin="none"
+                  type="number"
+                  onChange={updateNewQuantity}
+                  onKeyDown={saveNewItem}
+                  value={newItem.quantity || ''}
+                ></TextField>
+              </TableCell>
+            </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
     </main>
   )
 }
-
-function createData(name: string, quantity: number) {
-  return { name, quantity }
-}
-
-const rows = [createData('t-shirt', 4), createData('pants', 4)]
