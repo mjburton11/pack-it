0d89d0bd668504e7e4f2ec871df0a4eac8cbbc75
Michael Burton <mjburton11@Michaels-MacBook-Pro.local>
2023-03-15 23:10:08 -0400

    created dummy api

---
+++ api/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+// GET endpoint
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+// POST endpoint
+interface PostRequest {
+  name: string
+}
+
+app.post('/', (req: Request<{}, {}, PostRequest>, res: Response) => {
+  const { name } = req.body
+  res.send(`Hello ${name}!`)
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
